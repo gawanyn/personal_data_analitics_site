435bf88e53581988a6187d84eaea8f12be7b0df2
tarasshcheban <shcherbantaras07@gmail.com>
2024-08-31 21:39:24 +0300

    modify slideshow section

--- js/script.js
+++ js/script.js
@@ -1,19 +1,43 @@
-let slideIndex = 0;
-showSlides();
+let slideIndex = 1;
+let slideInterval; // Змінна для інтервалу
+showSlides(slideIndex);
+startSlideInterval();
 
-function showSlides() {
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+  resetSlideInterval(); // Перезапускає таймер
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+  resetSlideInterval(); // Перезапускає таймер
+}
+
+function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";  
   }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 3000); // Change image every 3 seconds
+}
+
+// Функція для запуску інтервалу
+function startSlideInterval() {
+  slideInterval = setInterval(function() {
+    plusSlides(1);
+  }, 3000); // Змінює слайд кожні 3 секунди
+}
+
+// Функція для зупинки і перезапуску інтервалу
+function resetSlideInterval() {
+  clearInterval(slideInterval); // Зупиняє поточний інтервал
+  startSlideInterval(); // Запускає новий інтервал
 }
